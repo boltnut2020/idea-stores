0a958a48e337d00fe70083fbc47e74a7ea75313d
nejimaki <nejishiki5@gmail.com>
2020-08-29 22:48:08 +0700

    typo model_has_permissions

--- database/migrations/2020_08_25_160441_create_permission_tables.php
+++ database/migrations/2020_08_25_160441_create_permission_tables.php
@@ -63,7 +63,7 @@ class CreatePermissionTables extends Migration
                 ->onDelete('cascade');
 
             $table->primary(['role_id', $columnNames['model_morph_key'], 'model_type'],
-                    ''model_has_roles_role_model_type_primary'model_has_roles_role_model_type_primary);
+                    'model_has_roles_role_model_type_primary');
         });
 
         Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
