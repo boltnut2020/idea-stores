e0890e2bf7c608c5117d9fee90aad03166a772e8
nejimaki <nejishiki5@gmail.com>
2020-09-04 22:57:57 +0700

    add vue component clock

---
+++ resources/js/components/ClockComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="clock-parent">
+    <vue-clock />
+  </div>
+</template>
+
+<script>
+import VueClock from '@dangvanthanh/vue-clock';
+
+export default {
+  components: { VueClock }
+}
+</script>
+
+<style scoped>
+.clock-parent >>> .clock {
+	margin-bottom: 0px;
+} 
+.clock-parent >>> .clock__hours,
+.clock-parent >>> .clock__minutes,
+.clock-parent >>> .clock__seconds {
+  font-size: 1.8rem;
+  position: relative;
+}
+</style>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Idea Stores') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Idea Stores') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -79,6 +79,9 @@
                 <div class="row">
                     <div class="col-md-3 col-sm-6 order-sm-12 order-12 order-md-1">
                         <div class="list-group">
+                            <clock-component></clock-component>
+                        </div>
+                        <div class="list-group mt-1">
                           <a href="/home" class="list-group-item list-group-item-action {{ (Request::segment(1) == "home") ? "active" : "" }}">
                             Home
                           </a>
