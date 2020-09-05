fe40b78948d7de19933804ec23e987bc4aac2943
nejimaki <nejishiki5@gmail.com>
2020-09-05 23:49:13 +0700

    adjustment memo htmltable

--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -10,17 +10,27 @@
       <thead class="thead-dark">
         <tr>
           <th scope="col">#</th>
-          <th scope="col">MEMO</th>
-          <th scope="col">ACTION</th>
+          <th scope="col" style="width: 95%">MEMO</th>
+          <!--<th scope="col">ACTION</th>-->
         </tr>
       </thead>
       <tbody>
         @foreach ($memos as $memo)
         <tr>
           <th scope="row"><i class="fas fa-list"></i></th>
-          <td class="text-break" style="width: 70%">{!! $memo->displayHtml() !!}
+          <td class="text-break">{!! $memo->displayHtml() !!}
             <div class="pt-1 small text-right">
-                    {{ $memo->created_at }}
+                {{ $memo->created_at }}
+                <a class="btn btn-light" href="/memos/create/{{$memo->id}}"><i class="fas fa-plus"></i></a>
+                <a class="btn btn-light" href="/memos/{{$memo->id}}/edit"><i class="fas fa-edit"></i></a>
+                <form class="d-inline" action="/memos/{{$memo->id}}" method="post">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button class="icon-button">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </form>
+
             </div>
             <div class="pt-1">
               @foreach ($memo->tags as $tag)
@@ -30,6 +40,7 @@
               @endforeach
             </div>
           </td>
+<!--
           <td>
             <a class="btn btn-light" href="/memos/create/{{$memo->id}}"><i class="fas fa-plus"></i></a>
             <a class="btn btn-light" href="/memos/{{$memo->id}}/edit"><i class="fas fa-edit"></i></a>
@@ -41,14 +52,23 @@
                 </button>
             </form>
     	  </td>
+-->
         </tr>
         @if (request()->path() == "memos/thread/list")
         @foreach ($memo->childrenRecursive as $children)
         <tr>
           <th scope="row"></th>
-          <td class="text-break" style="width: 70%">{!! $children->displayHtml() !!}
+          <td class="text-break" >{!! $children->displayHtml() !!}
             <div class="pt-1 small text-right">
-                    {{ $children->created_at }}
+                {{ $children->created_at }}
+                <a class="btn btn-light" href="/memos/{{$children->id}}/edit"><i class="fas fa-edit"></i></a>
+                <form class="d-inline" action="/memos/{{$children->id}}" method="post">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button class="icon-button">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </form>
             </div>
             <div class="pt-1">
               @foreach ($children->tags as $tag)
@@ -58,6 +78,7 @@
               @endforeach
             </div>
           </td>
+<!--
           <td>
             <a class="btn btn-light" href="/memos/{{$children->id}}/edit"><i class="fas fa-edit"></i></a>
             <form class="d-inline" action="/memos/{{$children->id}}" method="post">
@@ -68,6 +89,7 @@
                 </button>
             </form>
     	  </td>
+-->
         </tr>
         @endforeach
         @endif
