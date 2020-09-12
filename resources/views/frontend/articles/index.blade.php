2119321655bd1bce073f99058ab32aa94e53832c
nejimaki <nejishiki5@gmail.com>
2020-09-13 01:08:30 +0700

    adjust design

--- resources/views/frontend/articles/index.blade.php
+++ resources/views/frontend/articles/index.blade.php
@@ -5,18 +5,22 @@
 @section('content')
   @foreach ($articles as $article)
     <div class="card mb-3">
-        <div class="card-header text-right">
-        @foreach($article->categories as $c)
-            <a class="btn btn-secondary" href="{{ route('frontend.articles.category', ['category' => $c->id]) }}" >#{{ $c->name }}</a>
-        @endforeach
-        </div>
         <div class="card-body">
             <h2 class="card-title"><a class="text-dark" href="{{ route('frontend.articles.show', ['article' => $article->id]) }}">{{$article->title}}</a></h2>
-            <p class="card-text text-left">{{ $article->description }}</p>
-            <div class="text-right mb-2">
-                <a class="btn btn-light" href="{{ route('frontend.articles.show', ['article' => $article->id]) }}">{{ __('SHOW') }}</a>
+            <p class="card-text text-left mb-3">{{ $article->description }}</p>
+
+            <div class="row">
+                <div class="col-6">
+                    <p class="">
+                        @foreach($article->categories as $c)
+                            <a class="btn btn-light" href="{{ route('frontend.articles.category', ['category' => $c->id]) }}" >#{{ $c->name }}</a>
+                        @endforeach
+                    </p>
+                </div>
+                <div class="col-6">
+                    <p class="card-text text-right text-secondary">{{$article->created_at }}</p>
+                </div>
             </div>
-            <p class="card-text text-right text-secondary">{{$article->created_at }}</p>
       </div>
   </div>
   @endforeach
