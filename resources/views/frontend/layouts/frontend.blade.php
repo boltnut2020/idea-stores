b096ca3f4072082c34c2e64b0aa8cdf292b201fe
nejimaki <nejishiki5@gmail.com>
2020-09-20 04:55:37 +0700

    change service name

--- resources/views/frontend/layouts/frontend.blade.php
+++ resources/views/frontend/layouts/frontend.blade.php
@@ -31,7 +31,7 @@
         <nav class="text-white navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Idea Stores') }}
+                    {{ config('app.name', '次回、') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
